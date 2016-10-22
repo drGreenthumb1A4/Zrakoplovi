03d5a15d9f59b0c506ae49328cadebd7e4f231c0
drgreenthumb1a4 <filip.pilot@gmail.com>
2016-10-22 11:33:51 +0200

    initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string izborKoridoras;
+            ZracniKoridor[] brojKoridora = new ZracniKoridor[3];
+
+            ZracniKoridor iz = new ZracniKoridor();
+            ZracniKoridor sj = new ZracniKoridor();
+            ZracniKoridor oc = new ZracniKoridor();
+            brojKoridora[0] = iz;
+            brojKoridora[1] = sj;
+            brojKoridora[2] = oc;
+
+            do
+            {
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Zracni koridor broj 1: Istok-Zapad");
+                Console.WriteLine("Zracni koridor broj 2" + " Sjever-Jug");
+                Console.WriteLine("Zracni koridor broj 3" + " Prekooceanski");
+                Console.WriteLine("Unesi broj koridora");
+                Console.WriteLine("------------------------------------------");
+
+
+                izborKoridoras = Console.ReadLine();
+
+                int izbor = int.Parse(izborKoridoras) - 1;
+
+
+            } while (true);
+        }
+    }
+}
---
+++ ConsoleApplication2/ZracniKoridor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class ZracniKoridor
+    {
+        string ime;
+        int brojZrakoplova;
+        Zrakoplov[] zrakoplovi = new Zrakoplov[20];
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+            set
+            {
+                ime = value;
+            }
+        }
+        public int BrojZrakoplova
+        {
+            get
+            {
+                return brojZrakoplova;
+            }
+            set
+            {
+                brojZrakoplova = value;
+            }
+        }
+        public Zrakoplov[] listZrakoplova
+        {
+            get
+            {
+                return zrakoplovi;
+            }
+            set
+            {
+                zrakoplovi = value;
+            }
+        }
+        public bool DodajZrakoplov(Zrakoplov zrakoplov)
+        {
+            if(brojZrakoplova >= 20)
+            {
+                return false;
+            }
+            for(int i = 0; i<zrakoplovi.Length; i++)
+            {
+                if (zrakoplovi[i].visina == zrakoplov.visina && zrakoplovi[i] != null)
+                {
+                    return false;
+                }
+            }
+            for(int i = 0; i < zrakoplovi.Length; i++)
+            {
+                if(zrakoplovi[i] == null)
+                {
+                    zrakoplovi[i] = zrakoplov;
+                    brojZrakoplova++;
+                }
+            }
+
+            return true;
+        }
+        public bool UkloniZrakoplov(string reg)
+        {
+            for(int i = 0; i < zrakoplovi.Length; i++)
+            {
+                if(zrakoplovi[i].registracija == reg)
+                { 
+                    zrakoplovi[i] = null;
+                    brojZrakoplova--;
+                    return true;
+                }
+                
+            }
+            return true;
+        }
+        public void ispisiListuZrakoplova()
+        {
+            for (int i = 0; i < zrakoplovi.Length; i++)
+            {
+                Console.WriteLine("Zrakoplov broj " + i);
+                Console.WriteLine("Registracija: " + zrakoplovi[i].registracija);
+                Console.WriteLine("Destinacija: " + zrakoplovi[i].destinacija);
+                Console.WriteLine("Visina: " + zrakoplovi[i].visina);
+            }
+        }
+
+    }
+}
